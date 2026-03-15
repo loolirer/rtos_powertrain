8ea535c57abed9193df59b29fbcc32f1eadb9638
loolirer <loolirer@gmail.com>
2026-03-14 22:30:23 -0300

    add pid logic

--- FreeRTOS_Pico2W.cpp
+++ FreeRTOS_Pico2W.cpp
@@ -15,28 +15,33 @@ extern "C" {
 // Standard includes
 #include <stdio.h>
 
-#define SETPOINT_TASK "SETPOINT_TASK"
+#define SETPOINT_TASK "WIFI_SETPOINT"
 #define WIFI_SSID "Quarto"
 #define WIFI_PASSWORD "07055492"
 #define PORT 1234
-static QueueHandle_t setpoint_queue;
 
-#define MOTOR_TASK "MOTOR_TASK"
+#define MOTOR_CONTROLLER_TASK "MOTOR_CONTROLLER"
 #define N_MOTORS 2
 typedef struct {
     int motor_id;
     float target_speed;
     float measured_speed;
+    float Kp;
+    float Ki;
+    float Kd;
+    float dt;
+    int dt_ms;
     QueueHandle_t encoder_queue;
+    QueueHandle_t setpoint_queue;
 } MotorConfig_t;
 MotorConfig_t MotorControls[N_MOTORS];
 
-#define ENCODER_TASK "ENCODER_TASK"
+#define ENCODER_TASK "ENCODER"
 
-// Receives UDP string, parses to floats, and sends desired 
-// angular velocity of each wheel (in rad/s) to the motor queue
+// Receives UDP string, parses to floats, and routes desired 
+// angular velocity to the specific motor's queue
 void setpoint_task(void *pvParameters) {
-    QueueHandle_t setpoint_queue = (QueueHandle_t)pvParameters;
+    MotorConfig_t *motors = (MotorConfig_t *)pvParameters;
     float phi_dots[2] = {0.0f, 0.0f};
 
     if (cyw43_arch_init()) {
@@ -68,8 +73,7 @@ void setpoint_task(void *pvParameters) {
     printf("[%s] Listening for setpoints on UDP port %d...\n", SETPOINT_TASK, PORT);
 
     for ( ; ; ) {
-        int len = recvfrom(sock, buffer, sizeof(buffer) - 1, 0, 
-                           (struct sockaddr *)&client_addr, &addr_len);
+        int len = recvfrom(sock, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&client_addr, &addr_len);
                            
         if (len > 0) {
             buffer[len] = '\0';
@@ -77,7 +81,9 @@ void setpoint_task(void *pvParameters) {
             int parsed = sscanf(buffer, "%f %f", &phi_dots[0], &phi_dots[1]);
             if (parsed == 2) {
                 printf("[%s] Motor 0: %.2f rad/s | Motor 1: %.2f rad/s\n", SETPOINT_TASK, phi_dots[0], phi_dots[1]);
-                xQueueSend(setpoint_queue, &phi_dots, 0U);
+                
+                xQueueSend(motors[0].setpoint_queue, &phi_dots[0], 0U);
+                xQueueSend(motors[1].setpoint_queue, &phi_dots[1], 0U);
 
             } else {
                 printf("[%s] Ignored invalid UDP packet: '%s'.\n", SETPOINT_TASK, buffer);
@@ -87,30 +93,56 @@ void setpoint_task(void *pvParameters) {
 }
 
 // Actuate on motor
-void motor_task(void *pvParameters) {
+void motor_controller_task(void *pvParameters) {
     MotorConfig_t *MotorConfig = (MotorConfig_t *)pvParameters;
-    float setpoints[N_MOTORS];
+
+    float received_setpoint = 0.0f;
+
+    float error = 0.0f;
+    float previous_error = 0.0f;
+    float accumulated_error = 0.0f;
+    const float integral_max = 50.0f; 
+    const float interal_min = -50.0f;
+
+    const TickType_t xFrequency = pdMS_TO_TICKS(MotorConfig->dt_ms); 
+    TickType_t xLastWakeTime = xTaskGetTickCount();
 
     for( ; ; ) {
-        if(xQueueReceive(setpoint_queue, &setpoints, 0U) == pdPASS) {
-            MotorConfig->target_speed = setpoints[MotorConfig->motor_id];
+        if(xQueueReceive(MotorConfig->setpoint_queue, &received_setpoint, 0U) == pdPASS) {
+            MotorConfig->target_speed = received_setpoint;
         }
-
         xQueueReceive(MotorConfig->encoder_queue, &MotorConfig->measured_speed, 0U);
-        printf(
-            "[MOTOR_%d]: Trying %f rad/s | Actually at %f rad/s\n", 
-            MotorConfig->motor_id, 
-            MotorConfig->target_speed,
-            MotorConfig->measured_speed
-        );
-        vTaskDelay(500);
+
+        error = MotorConfig->target_speed - MotorConfig->measured_speed;
+        float P = MotorConfig->Kp * error;
+        accumulated_error = accumulated_error + (error * MotorConfig->dt);
+        if (accumulated_error > integral_max) {
+            accumulated_error = integral_max;
+        } else if (accumulated_error < interal_min) {
+            accumulated_error = interal_min;
+        }
+        float I = MotorConfig->Ki * accumulated_error;
+        float D = MotorConfig->Kd * (error - previous_error) / MotorConfig->dt;
+        previous_error = error;
+        float control_signal = P + I + D;
+
+        if (control_signal > 100.0f) control_signal = 100.0f;
+        if (control_signal < -100.0f) control_signal = -100.0f;
+
+        //TODO: PWM LOGIC
+        
+        printf("[%s_%d] Target: %.2f | Measured: %.2f | PWM Output: %.2f\n", 
+               MOTOR_CONTROLLER_TASK, MotorConfig->motor_id, 
+               MotorConfig->target_speed, MotorConfig->measured_speed, control_signal);
+
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
 }
 
 // Reads encoder and sends the angular velocity 
 // of the wheel in rad/s
 void encoder_task(void *pvParameters) {
-    float phi_dot = 0.0;
+    float phi_dot = 0.0f;
     MotorConfig_t *MotorConfig = (MotorConfig_t *)pvParameters;
 
     for( ; ; ) {
@@ -121,17 +153,21 @@ void encoder_task(void *pvParameters) {
 
 int main() {
     stdio_init_all(); // From Pico stdlib
-    
-    setpoint_queue = xQueueCreate(1, sizeof(float[2]));
-    xTaskCreate(setpoint_task, SETPOINT_TASK, 1024, (void*)setpoint_queue, 2, NULL);
 
+    xTaskCreate(setpoint_task, SETPOINT_TASK, 1024, (void*)MotorControls, 2, NULL);
     for(int i = 0; i < N_MOTORS; i++) {
         MotorControls[i].motor_id = i;
-        MotorControls[i].target_speed = 0.0;
-        MotorControls[i].measured_speed = 0.0;
+        MotorControls[i].target_speed = 0.0f;
+        MotorControls[i].measured_speed = 0.0f;
+        MotorControls[i].Kp = 5.0f;
+        MotorControls[i].Ki = 2.0f;
+        MotorControls[i].Kd = 0.0f;
+        MotorControls[i].dt_ms = 10;
+        MotorControls[i].dt = float(MotorControls[i].dt_ms) / 1000.0f;
+        MotorControls[i].setpoint_queue = xQueueCreate(1, sizeof(float));
         MotorControls[i].encoder_queue = xQueueCreate(1, sizeof(float));
         xTaskCreate(encoder_task, "ENCODER", 256, &MotorControls[i], 1, NULL);
-        xTaskCreate(motor_task, "MOTOR", 256, &MotorControls[i], 2, NULL);
+        xTaskCreate(motor_controller_task, "MOTOR", 256, &MotorControls[i], 2, NULL);
     }
 
     vTaskStartScheduler();
